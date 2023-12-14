73f4e5092255fdbfdc1f43371f06c170b3392070
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-12-14 20:16:02 +0600

    react-form-handle

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react"
+
 export default function Login() {
+    const [login, setLogin] = useState({
+        email: "",
+        password: ""
+    });
+    const handleInput = (e) => {
+        const { value, name } = e.target;
+        setLogin({ ...login, [name]: value });
+    }
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(login);
+    }
     return <>
         <section className="container register">
             <div>
@@ -10,9 +24,20 @@ export default function Login() {
             </div>
             <div>
                 <h1>Login</h1>
-                <form className="register_form">
-                    <input type="email" placeholder="Your Email" />
-                    <input type="password" placeholder="Your Password" />
+                <form onSubmit={submit} className="register_form">
+                    <input
+                        onChange={handleInput}
+                        name="email"
+                        value={login.email}
+                        type="email"
+                        placeholder="Your Email" />
+                    <input
+                        onChange={handleInput}
+                        name="password"
+                        value={login.password}
+                        type="password"
+                        placeholder="Your Password"
+                        autoComplete="on" />
                     <button type="submit">Login</button>
                 </form>
             </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export default function Register() {
+    const [register, setRegister] = useState({
+        name: "",
+        email: "",
+        username: "",
+        password: ""
+    });
+    const handleInput = (e) => {
+        const { value, name } = e.target;
+        setRegister({ ...register, [name]: value });
+    }
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(register);
+    }
     return <>
         <section className="container register">
             <div>
@@ -10,11 +26,32 @@ export default function Register() {
             </div>
             <div>
                 <h1>Become an Author</h1>
-                <form className="register_form">
-                    <input type="text" placeholder="Your Full Name" />
-                    <input type="email" placeholder="Your Email" />
-                    <input type="text" placeholder="Choose an Username" />
-                    <input type="password" placeholder="Choose a Password" />
+                <form onSubmit={submit} className="register_form">
+                    <input
+                        name="name"
+                        onChange={handleInput}
+                        value={register.name}
+                        type="text"
+                        placeholder="Your Full Name" />
+                    <input
+                        name="email"
+                        onChange={handleInput}
+                        value={register.email}
+                        type="email"
+                        placeholder="Your Email" />
+                    <input
+                        name="username"
+                        onChange={handleInput}
+                        value={register.username}
+                        type="text"
+                        placeholder="Choose an Username" />
+                    <input
+                        name="password"
+                        onChange={handleInput}
+                        value={register.password}
+                        type="password"
+                        placeholder="Choose a Password"
+                        autoComplete="on" />
                     <button type="submit">Register</button>
                 </form>
             </div>
