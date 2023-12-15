7349c7f304ea12d0463f1623c5fa5ae7860be589
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-12-16 05:11:39 +0600

    profile-header

--- client/src/components/ProfileHeader.jsx
+++ client/src/components/ProfileHeader.jsx
@@ -1,4 +1,7 @@
+import { useAuth } from "../store/auth";
+
 export default function ProfileHeader() {
+    const { user } = useAuth();
     return <section className="profile">
         <div className="profile_blogs_count">
             <h1>15</h1>
@@ -10,8 +13,8 @@ export default function ProfileHeader() {
                 draggable="false"
                 className="profile_avatar"
             />
-            <h1>Cool Cat</h1>
-            <p>@coolcat</p>
+            <h1>{user && user.name}</h1>
+            <p>@{user && user.username}</p>
         </div>
         <div className="profile_follower_count">
             <h1>10k</h1>
