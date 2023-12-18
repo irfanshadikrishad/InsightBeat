2a331e6a41ce5f07da1ff409aaa7507f99c43b40
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-12-18 22:04:08 +0600

    deploy - 3

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://insight-beat.vercel.app",
     methods: "*",
   })
 );
